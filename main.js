b951ac1fe63621d27e516c0b5c8cc237fab832ce
Rodrigo Santos Oliveira <rodrigoliveirasantos11@gmail.com>
2024-09-26 09:11:37 -0300

    feat: Commit inicial

---
+++ main.js
@@ -0,0 +1,138 @@
+class Operator {
+    constructor(token, precedence, args, handle) {
+        this.token = token;
+        this.precedence = precedence;
+        this.args = args;
+        this.handle = handle;
+    }
+}
+class Stack {
+    constructor(items = []) {
+        this.items = [];
+        for (let i = 0; i < items.length; i++) {
+            this.push(items[i]);
+        }
+    }
+    length() {
+        return this.items.length;
+    }
+    empty() {
+        return this.items.length === 0;
+    }
+    push(item) {
+        this.items.push(item);
+    }
+    top() {
+        if (!this.items.length) {
+            return undefined;
+        }
+        return this.items[this.items.length - 1];
+    }
+    pop() {
+        return this.items.pop();
+    }
+    toString() {
+        return JSON.stringify(this.items);
+    }
+}
+function makeOperatorsMap(operators) {
+    return operators.reduce((acc, operator) => {
+        return {
+            ...acc,
+            [operator.token]: operator
+        };
+    }, {});
+}
+const operatorsMap = makeOperatorsMap([
+    new Operator('+', 1, 2, (a, b) => a | b),
+    new Operator('.', 2, 2, (a, b) => a & b),
+    new Operator('^', 3, 2, (a, b) => a ^ b),
+    new Operator("'", 4, 1, (a) => a ? 0 : 1),
+]);
+function cleanUpExpression(exp) {
+    return exp.trim().replace(/[\u0020\n]+/g, '');
+}
+function inversePolishNotation(input, operators) {
+    const output = [];
+    const operatorsStack = new Stack();
+    console.log(">> Input: " + input);
+    if (!input) {
+        return [];
+    }
+    for (let i = 0; i < input.length; i++) {
+        const token = input.charAt(i);
+        const newOperator = operators[token];
+        const isOperator = !!newOperator;
+        console.log(">> Token: " + token);
+        console.log(">> Stack: " + operatorsStack.toString());
+        console.log(">> Out: " + JSON.stringify(output));
+        console.log();
+        // Qualquer não operador será considerado uma entrada ou valor fixo.
+        if (!isOperator) {
+            output.push(token);
+            continue;
+        }
+        while (!operatorsStack.empty()) {
+            const topOperator = operatorsStack.top();
+            // Achou o lugar certo do operador
+            if (topOperator.precedence < newOperator.precedence) {
+                break;
+            }
+            // Move o operador que estava no topo para o output
+            output.push(topOperator);
+            operatorsStack.pop();
+        }
+        operatorsStack.push(newOperator);
+    }
+    while (!operatorsStack.empty()) {
+        output.push(operatorsStack.pop());
+    }
+    return output;
+}
+function solver(input, context) {
+    const solutionStack = new Stack();
+    console.log(">> Resolvendo: ", input);
+    console.log(">> Contexto: ", context);
+    for (let i = 0; i < input.length; i++) {
+        const symbol = input[i];
+        // Se passar é um operando.
+        if (typeof symbol === 'string') {
+            if (symbol === '0' || symbol === '1') {
+                solutionStack.push(Number(symbol));
+            }
+            else if (context.has(symbol)) {
+                solutionStack.push(context.get(symbol));
+            }
+            else {
+                return {
+                    error: `Variável ${symbol} não tem valor definido.`
+                };
+            }
+            continue;
+        }
+        // Carrega os argumentos da operação
+        const operationArgs = [];
+        for (let j = 0; j < symbol.args; j++) {
+            if (solutionStack.empty()) {
+                return {
+                    error: 'Não existem valores o suficiente para a operação ' + symbol.token
+                };
+            }
+            operationArgs.push(solutionStack.pop());
+        }
+        solutionStack.push(symbol.handle(...operationArgs));
+    }
+    if (solutionStack.length() > 1) {
+        return {
+            error: 'Expressão invalida. Dados não foram processados.'
+        };
+    }
+    return { solution: solutionStack.pop() };
+}
+const context = new Map([
+    ['A', 0],
+    ['B', 1],
+    ['C', 0]
+]);
+console.log(solver(inversePolishNotation(cleanUpExpression("'A +"), // Espera 
+operatorsMap), context));
